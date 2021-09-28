67e3b851c2a46dec1978f33cc786948263c0a719
geonwoo KIM <howkk@naver.com>
2021-09-28 22:22:52 +0900

    aws update

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,4 +1,28 @@
 import multer from "multer"
+import multerS3 from "multer-s3"
+import aws from "aws-sdk"
 
-export const uploadAvatar=multer({dest:"uploads/avatar"})
-export const uploadVideo=multer({dest:"uploads/video"})
+const s3 = new aws.S3({
+    credentials :{
+        accessKeyId:process.env.AWS_ID,
+        secretAccessKey:process.env.AWS_SECRET
+    }
+})
+const multerUploaer= multerS3({
+    s3: s3,
+    bucket : "milktube"
+})
+export const uploadAvatar=multer({dest:"uploads/avatar",
+storage:multerUploaer,
+limits:{
+    fileSize:3000000,
+}
+
+})
+export const uploadVideo=multer({dest:"uploads/video",
+storage:multerUploaer,
+limits:{
+    fileSize:10000000,
+}
+
+})
